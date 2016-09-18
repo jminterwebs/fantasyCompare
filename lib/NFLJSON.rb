bb3e8aade1ec270461a4d071e35e5ae49f30ad1f
John Verdone <jmverdone@gmail.com>
2016-09-18 19:09:19 -0400

    some basic testing anf figuring stuff out

--- lib/NFLJSON.rb
+++ lib/NFLJSON.rb
@@ -1,3 +1,21 @@
-class NFL
+require 'httparty'
 
+require 'pry'
+
+
+
+url = 'http://api.fantasy.nfl.com/v1/players/stats?statType=weekStats&season=2016&week=1&position=QB&format=json&returnHTML=1'
+
+response = HTTParty.get(url)
+test = response.parsed_response
+
+def players(url)
+  player = []
+  url["players"].each do |key, value|
+    player << key["name"]
+  end
+  player
+  binding.pry
 end
+
+  players(test)
