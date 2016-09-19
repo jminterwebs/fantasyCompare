2c3450bc451b5dfeb5e9e2368fa982996c11c1d2
John Verdone <jmverdone@gmail.com>
2016-09-18 21:06:07 -0400

    basic user input working and simple hash return

--- lib/NFLJSON.rb
+++ lib/NFLJSON.rb
@@ -1,21 +1,37 @@
 require 'httparty'
-
 require 'pry'
 
 
+class NFLJSON
 
-url = 'http://api.fantasy.nfl.com/v1/players/stats?statType=weekStats&season=2016&week=1&position=QB&format=json&returnHTML=1'
-
-response = HTTParty.get(url)
-test = response.parsed_response
-
-def players(url)
-  player = []
-  url["players"].each do |key, value|
-    player << key["name"]
+# Takes input to parse proper JSON Position data by returing proper url link
+def self.inputs
+  positions = ["QB","RB","WR","TE","K","DEF"]
+  puts "Please pick a position (1-6) \n1.QB\n2.RB\n3.WR\n4.Te\n5.K\n6.DEF"
+  pos_input = gets.chomp
+  pos_input = pos_input.to_i-1
+  if !pos_input.between?(0,5)
+    puts "Invalid Input"
+    inputs
+  else
+    @position = positions[pos_input]
   end
-  player
-  binding.pry
+  @url =  "http://api.fantasy.nfl.com/v1/players/stats?statType=weekStats&season=2016&week=1&position=#{@position}&format=json&returnHTML=1"
 end
 
-  players(test)
+def self.players(url)
+  response = HTTParty.get(url)
+  test = response.parsed_response
+  players = []
+  test["players"].each do |key, value|
+    weekProjectedPts = key["weekProjectedPts"]
+    name = key["name"]
+    players << {name: name, weekProjectedPts: weekProjectedPts}
+  end
+  players
+end
+
+
+  binding.pry
+
+end
