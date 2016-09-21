689bd3e270fa042c6c484c70130f20fb5225675a
John Verdone <jmverdone@gmail.com>
2016-09-20 21:24:03 -0400

    added method to sort list and return top 10 players

--- lib/NFLJSON.rb
+++ lib/NFLJSON.rb
@@ -3,13 +3,14 @@ require 'pry'
 
 
 class NFLJSON
-  attr_accessor :position
+  attr_accessor :position, :week, :players
+
 
   #take a url with variables to get proper JSON data
-  def self.url(position)
-    @url =  "http://api.fantasy.nfl.com/v1/players/stats?statType=weekStats&season=2016&week=1&position=#{position}&format=json&returnHTML=1"
+  def self.url(position, week)
+    @url =  "http://api.fantasy.nfl.com/v1/players/stats?statType=weekProjectedStats&season=2016&week=#{week}&position=#{position}&format=json&returnHTML=1"
   end
-    
+
 # Takes input to parse proper JSON Position data by returing proper url link
 def self.inputs
   positions = ["QB","RB","WR","TE","K","DEF"]
@@ -22,22 +23,33 @@ def self.inputs
   else
     @position = positions[pos_input]
   end
-  self.url(@position)
+    puts "Please select a week for stats"
+    @week = gets.chomp
+  self.url(@position, @week)
   self.players(@url)
 end
 
+#generates simplefied list of parsed down data from API
 def self.players(url)
   ###move out
   response = HTTParty.get(url)
   test = response.parsed_response
-  players = []
+  @players = []
   test["players"].each do |key, value|
     weekProjectedPts = key["weekProjectedPts"]
     name = key["name"]
     playerId = key["id"]
-    players << {name: name, weekProjectedPts: weekProjectedPts, playerId: playerId}
+    @players << {name: name, weekProjectedPts: weekProjectedPts, playerId: playerId}
   end
-  players
+  @players
 end
+
+  #take the parsed players hash array, sorts it and returns the top 10 projected players
+ def self.top_ten_list(players)
+   @top_ten = []
+   @top_ten << players.sort_by {|key| key[:weekProjectedPts]}.reverse!.first(10)
+
+ end
+
   binding.pry
 end
